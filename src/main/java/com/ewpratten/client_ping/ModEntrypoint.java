78ea403721c7f2624d57b93982ab74aa97a3cc5e
bq0062 <bq0062>
2024-03-31 17:43:03 +0200

    Sounds added for pings and anti backclaim system working with sounds

--- src/main/java/com/ewpratten/client_ping/ModEntrypoint.java
+++ src/main/java/com/ewpratten/client_ping/ModEntrypoint.java
@@ -1,13 +1,23 @@
 package com.ewpratten.client_ping;
 
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 public class ModEntrypoint implements ModInitializer {
+	public static final Identifier soundId = new Identifier("client_ping", "ping_sound");
+	public static final Identifier soundId2 = new Identifier("client_ping", "ping_sound2");
+	public static final SoundEvent soundEvent = SoundEvent.createFixedRangeEvent(soundId, 1.0F);
+	public static final SoundEvent soundEvent2 = SoundEvent.createFixedRangeEvent(soundId2, 1.0F);
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		Globals.LOGGER.info("Mod Loaded. Today will be a great day!");
+		Registry.register(Registries.SOUND_EVENT, soundId, soundEvent);
+		Registry.register(Registries.SOUND_EVENT, soundId2, soundEvent2);
 	}
 
 }
--- src/main/java/com/ewpratten/client_ping/mixin/InGameHudMixin.java
+++ src/main/java/com/ewpratten/client_ping/mixin/InGameHudMixin.java
@@ -1,11 +1,20 @@
 package com.ewpratten.client_ping.mixin;
 
+import com.ewpratten.client_ping.ModEntrypoint;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatMessageTag;
+import net.minecraft.client.sound.Sound;
+import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.network.message.MessageSignature;
 
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registry;
+
+import net.minecraft.util.ModStatus;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +26,8 @@ import com.ewpratten.client_ping.logic.PingRegistry;
 
 @Mixin(ChatHud.class)
 public class InGameHudMixin {
-
+	MinecraftClient player = MinecraftClient.getInstance();
+	SoundEvent sound;
 	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignature;Lnet/minecraft/client/gui/hud/ChatMessageTag;)V", cancellable = true)
 	public void addMessage(Text message, MessageSignature signature, ChatMessageTag tag, CallbackInfo info) {
 
@@ -25,18 +35,19 @@ public class InGameHudMixin {
 		String messageString = message.getString();
 		String[] parts = messageString.split(" ", 3);
 
-
 		if (parts.length < 3) {
 			return; // No es el formato esperado, retorna temprano
 		}
 		String chatBody = "";
 		String username = "";
 		if(parts[0].startsWith("[War]")){
+			String city2 = "";
 			boolean matchFound = false;
-			String[] cities = {"Kabul", "Delhi", "Mumbai, Amsterdam"};
+			String[] cities = {"Catalan", "Murcia", "Balearic_Islands", "Vascongadas", "Leon", "Seville", "Madrid", "Andalusia", "Havana", "Santiago", "Manile"};
 			String[] parts2 = messageString.split(" ");
 			for (String city : cities){
 				if (city.equals(parts2[4])) {
+					city2 = city;
 					matchFound = true;
 					break;
 				}
@@ -45,8 +56,12 @@ public class InGameHudMixin {
 				String coordinatesPart = messageString.split("at ")[1];
 				coordinatesPart = coordinatesPart.replaceAll("[()]", "");
 				String[] coordinates = coordinatesPart.split(", ");
-				chatBody = String.format("Ping at {minecraft:overworld:(%d, %d, %d)}", Integer.parseInt(coordinates[0].trim(), Integer.parseInt(coordinates[1].trim(), Integer.parseInt(coordinates[2].trim()))));
-				username = "Ataque";
+				chatBody = String.format("Ping at {minecraft:overworld:(%s, %s, %s)}",
+					coordinates[0].trim(),
+					coordinates[1].trim(),
+					coordinates[2].trim());
+				username = city2;
+				sound = ModEntrypoint.soundEvent2;
 			}
 		}
 		else {
@@ -54,11 +69,19 @@ public class InGameHudMixin {
 
 // El cuerpo del mensaje es la tercera parte después de dividir por el segundo espacio
 			chatBody = parts[2];
+			sound = ModEntrypoint.soundEvent;
+
+
 		}
 		Ping parseResult = Ping.deserialize(chatBody, username);
 		if (parseResult == null) {
 			return;
 		}
+		else {
+			player.player.playSound(sound, SoundCategory.PLAYERS, 1.0F,1.0F);
+		}
+
+
 // El nombre de usuario estaría en la segunda parte, después de omitir el prefijo [Town]/[Nation]
 // Además, eliminamos los dos puntos al final si están presentes
 
